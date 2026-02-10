b5df50caa7931e758da21d6aa5acd756b459b8cc
paulom0q <105054182+PauloMoq@users.noreply.github.com>
2026-02-10 16:20:38 +0100

    TP1: soluce 2.3

--- project/main.py
+++ project/main.py
@@ -4,13 +4,19 @@ import math
 if __name__ == '__main__':
     bob = RobotMobile(6, 7, math.pi/2)
 
-    print("Bob: ", bob)
-
-    bob.avancer(3)
-    print("Bob moved to: ", bob)
-
-    bob.tourner(30)
-    print("Bob rotated to: ", bob)
-
+    #print("Bob: ", bob)
+    #bob.avancer(3)
+    #print("Bob moved to: ", bob)
+    #bob.tourner(30)
+    #print("Bob rotated to: ", bob)
 
     #pour rep à la question 2.3
+    print("\n(2.3) answer:")
+    karim=RobotMobile(0, 0, 0)
+    print("Karim: ", karim)
+
+    karim.avancer(1)
+    print("Karim moved to: ", karim)
+
+    karim.tourner(45)
+    print("Karim rotated to: ", karim)
